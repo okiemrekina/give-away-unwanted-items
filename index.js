5a2c03168e956df05998efe703e4d02426035939
Emilia Miller <emilia.miller@epicode.net>
2018-12-23 00:52:08 +0100

    app setup

---
+++ index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import './js/font.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
